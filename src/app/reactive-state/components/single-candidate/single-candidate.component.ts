cffa007308da629bcaddd415cd0cff66ec884456
Jonathan <jonathangager75@gmail.com>
2025-01-09 12:13:57 +0100

    Prepare module and routes setup

---
+++ src/app/reactive-state/components/single-candidate/single-candidate.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-single-candidate',
+  imports: [],
+  templateUrl: './single-candidate.component.html',
+  styleUrl: './single-candidate.component.scss'
+})
+export class SingleCandidateComponent {
+
+}
