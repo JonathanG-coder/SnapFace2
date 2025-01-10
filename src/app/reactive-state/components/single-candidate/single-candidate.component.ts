1a589591aba009650ebbdcb8884794df656feba9
Jonathan <jonathangager75@gmail.com>
2025-01-10 11:10:04 +0100

    Collect a single candidate

--- src/app/reactive-state/components/single-candidate/single-candidate.component.ts
+++ src/app/reactive-state/components/single-candidate/single-candidate.component.ts
@@ -1,12 +1,65 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Observable, switchMap } from 'rxjs';
+import { Candidate } from '../../models/candidate.model';
+import { CandidatesService } from '../../services/candidates.service';
+import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-single-candidate',
-  imports: [],
+  standalone: true,
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatProgressSpinnerModule,
+    MatListModule,
+    MatButtonModule,
+    RouterModule
+  ],
   templateUrl: './single-candidate.component.html',
   styleUrl: './single-candidate.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class SingleCandidateComponent {
+
+
+  loading$!: Observable<boolean>;
+  candidate$!: Observable<Candidate>;
+
+  constructor(private candidatesService: CandidatesService,
+    private route: ActivatedRoute,
+    private router: Router,
+  ) { }
+
+  ngOnInit(): void {
+    this.initObservables();
+  }
+
+  private initObservables() {
+    this.loading$ = this.candidatesService.loading$;
+    this.candidate$ = this.route.params.pipe(
+      switchMap(params => this.candidatesService.getCandidateById(+params['id']))
+    );
+  }
+
+  onGoBack() {
+    this.router.navigateByUrl('/reactive-state/candidates');
+  }
+
+  onRefuse() {
+    throw new Error('Method not implemented.');
+  }
+  
+  onHire() {
+    throw new Error('Method not implemented.');
+  }
+
+
+
+
 
 }
--- src/app/reactive-state/services/candidates.service.ts
+++ src/app/reactive-state/services/candidates.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { BehaviorSubject, delay, Observable, tap } from 'rxjs';
+import { BehaviorSubject, delay, map, Observable, tap } from 'rxjs';
 import { Candidate } from '../models/candidate.model';
 import { environment } from '../../../environnement/environment';
 
@@ -18,6 +18,9 @@ export class CandidatesService {
         return this._candidates$.asObservable();
     }
 
+    private lastCandidatesLoad = 0;
+
+
     private setLoadingStatus(loading: boolean) {
         this._loading$.next(loading);
     }
@@ -25,16 +28,28 @@ export class CandidatesService {
 
 
     getCandidatesFromServer() {
+        if (Date.now() - this.lastCandidatesLoad <= 300000) {
+            return;
+        }
         this.setLoadingStatus(true);
         this.http.get<Candidate[]>(`${environment.apiUrl}candidates`).pipe(
             delay(1000),
             tap(candidates => {
+                this.lastCandidatesLoad = Date.now();
                 this._candidates$.next(candidates);
                 this.setLoadingStatus(false);
             })
         ).subscribe();
     }
 
-
+    getCandidateById(id: number): Observable<Candidate> {
+        if (!this.lastCandidatesLoad) {
+            this.getCandidatesFromServer();
+        }
+        return this.candidates$.pipe(
+            map(candidates => candidates.filter(candidate => candidate.id === id)[0])
+        );
+    }
 
 }
+
